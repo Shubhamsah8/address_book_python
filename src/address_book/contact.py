0456449c78b2fea7566a32fe22f496d0892dc798
Shubhamsah8 <shubhamsah086@gmail.com>
2024-05-11 12:47:01 +0530

    [Shubham] Modify: To initialize the details of the contacts

--- src/address_book/contact.py
+++ src/address_book/contact.py
@@ -1,4 +1,24 @@
+#Class contact to initialize the details of the contact
+
 class Contact:
+    """
+        Initializing the details of the contact
+
+        Parameters:
+            self: Default parameter for the method inside the class
+            first_name (str): First name of the user
+            last_name (str): Last name of the user
+            address (str): Address of the user
+            city (str): City in which user live
+            state (str): State in which user stay
+            zip_code (str): Zip code of the address
+            phone_number (str): Phone number of the user
+            email (str): Email of the user
+
+        Returns:
+            None
+    """
+
     def __init__(self, first_name, last_name, address, city, state, zip_code, phone_number, email):
         self.first_name = first_name
         self.last_name = last_name
