5af79cfdf88b2c6d848d63327f52c4a1584f7e2b
Shubhamsah8 <shubhamsah086@gmail.com>
2024-05-11 12:46:31 +0530

    [Shubham] Modify: To store those details into the address book

--- src/address_book/address_book.py
+++ src/address_book/address_book.py
@@ -1,9 +1,39 @@
+# Class to initialize the list, add the details of the contacts in the list and display it
 class AddressBook:
+    """
+        To initialize the list
+
+        Parameters:
+            self: Default parameter in the method inside the class
+
+        Returns:
+            None
+    """
     def __init__(self):
         self.contacts = []
 
+    """
+        To add the detials of the contact into the list
+        
+        Parameters:
+            self: Default parameter
+            contact: The list in which the details of the contact get stored
+            
+        Returns:
+            None
+    """
     def add_contact(self, contact):
         self.contacts.append(contact)
+
+    """
+        Displaying the contacts that has been added into the list
+        
+        Parameters:
+            self: Default parameter
+            
+        Returns:
+            None
+    """
 
     def display_contacts(self):
         for contact in self.contacts:
